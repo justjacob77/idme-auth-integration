f4bba7c008ad741ac6fc3e592d30cce82eca4569
justjacob77 <jacobsearls@Gmail.com>
2024-12-13 10:46:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("idme-auth-button").addEventListener("click", () => {
     const clientId = "28bf5c72de76f94a5fb1d9454e347d4e";
-    const redirectUri = encodeURIComponent("https://justjacob77.github.io/idme-auth-integration/callback.html");
+    const redirectUri = encodeURIComponent("http://localhost:5500/callback.html");
     const authorizationEndpoint = "https://api.id.me/oauth/authorize";
     const scope = "login";
 
